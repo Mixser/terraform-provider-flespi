77a75f2d43525bc730e1d17db084f19aec7b3bd1
Mikhail Turchunovich <mixser.by@gmail.com>
2023-12-19 01:20:27 +0200

    Generate docs

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -1,23 +1,23 @@
 terraform {
-    required_providers {
-        flespi = {
-            source = "hashicorp.com/edu/flespi"
-        }
+  required_providers {
+    flespi = {
+      source = "hashicorp.com/edu/flespi"
     }
+  }
 }
 
 provider "flespi" {
-    token = "AlY..."
+  token = "AlY..."
 }
 
 
 resource "flespi_limit" "edu" {
-    name = "name of the limit"
-    webhooks_count = 23
-    cdn_storage = 12
+  name           = "name of the limit"
+  webhooks_count = 23
+  cdn_storage    = 12
 }
 
 resource "flespi_subaccount" "mitu" {
-  name = "mitu"
-  limit_id = flespi_limit.edu.id 
+  name     = "mitu"
+  limit_id = flespi_limit.edu.id
 }
